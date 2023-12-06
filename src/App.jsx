5929ef424e18d7bfb86e9fc3c8419986bafd12bd
Caleb Hefty <caleb.hefty@gmail.com>
2023-12-06 14:01:25 -0600

    Added disclaimer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Map from "./components/Map";
 import NavBar from "./components/NavBar";
 import AboutPanel from "./components/AboutPanel";
+import Disclaimer from "./components/Disclaimer";
 function App() {
   const [aboutToggled, setAboutToggled] = useState(false);
   const handleAboutToggle = () => {
@@ -12,6 +13,7 @@ function App() {
   return (
     <div>
       <NavBar onButtonClicked={handleAboutToggle} />
+      <Disclaimer />
       <AboutPanel aboutToggled={aboutToggled} />
       <Map />
     </div>
